478141558168898a7dc372ad94bc8464a3dce0d1
CalenDario13 <dariorusso.dr13@gmail.com>
2020-09-09 00:01:35 +0200

    NUOVO FILE! header.py Inseriti qui i vostri path. Qusto file non verrà più aggiornato su git...

---
+++ header.py
@@ -0,0 +1,3 @@
+PATH_DIRECTORY = '/Users/dario/Google Drive/DS/First Year - Secon Semester/SL/final_project/'
+
+PATH_FILE = '/Users/dario/Google Drive/DS/First Year - Secon Semester/SL/final_project/birds'
